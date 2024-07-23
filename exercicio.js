ff8c4d531abd70d17628f9227b0d8097991aae46
Felipe Simari <felipe_simari@hotmail.com>
2024-07-23 13:59:47 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+// Função de multiplicação
+function multiply(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(name) {
+    return `Olá ${name}`;
+}
+console.log(multiply(3, 4));
+console.log(saudacao("Alice"));
---
+++ exercicio.ts
@@ -0,0 +1,15 @@
+// Função de multiplicação
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(name: string): string {
+    return `Olá ${name}`;
+}
+
+
+
+console.log(multiply(3, 4));
+
+console.log(saudacao("Alice")); 
